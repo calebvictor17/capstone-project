ae1617cabcaa6704f53b8807ce5723fe9eac9536
calebvictor17 <calebusman710@gmail.com>
2025-03-23 22:43:47 +0100

    feat:created the contact-page and sytled

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-function page() {
+const page = () => {
   return (
-    <div>This is the contact page</div>
+    <section>
+      <div>
+      <div
+        style={{
+          backgroundImage: "url('/background 4.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "55vh",
+          transition: "all 1s",
+        }}
+      >
+        <h1 className="text-center text-white text-5xl font-extrabold pt-60">
+          CONTACT US
+        </h1>
+        <hr className="w-20 mx-auto border-2 border-red-600 mt-2" />
+      </div>
+      </div>
+    </section>
   )
 }
 
